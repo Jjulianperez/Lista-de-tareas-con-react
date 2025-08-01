0a96abbac95111602a95a059021727d55dc5b57c
Jjulianperez <julianjoseperez45@gmail.com>
2025-08-01 18:04:33 -0300

    Edite la app.jsx y agregue algunos stylos

--- to-do-list-react/src/App.jsx
+++ to-do-list-react/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { SpamTarea } from './components/SpamTarea'
+import { ContMjs } from './components/ContMsj'
 function App() {
   return (
     <>
       <section className='content-tareas'>
+        <ContMjs mensaje={"Hola Irina"}/>
         <SpamTarea tarea={"Pasear al perro"}/>  
         <SpamTarea tarea={"Comprar alimento"}/>  
-        <SpamTarea tarea={"Besar un arbol lindo"}/>
+        <SpamTarea tarea={"comprar flores a iri"}/>
       </section>
     </>  
   )
---
+++ to-do-list-react/src/components/ContMsj.jsx
@@ -0,0 +1,5 @@
+import './ContMsj.css'
+
+export const ContMjs = ({mensaje}) =>{
+    return(<h1 className='mensaje'>{mensaje}</h1>)
+}
